a732dadff00c0978ea2f00f6da319590d8c0c540
makechinafreedom <83006506+makechinafreedom@users.noreply.github.com>
2022-01-29 02:45:08 -0400

    Update search-notion.ts

--- lib/search-notion.ts
+++ lib/search-notion.ts
@@ -4,4 +4,4 @@ import pMemoize from 'p-memoize'
 
 import { api } from './config'
 import * as types from './types'
-export const searchNotion = pMemoize(searchNotionImpl, { maxAge: 10000 })
+// export const searchNotion = pMemoize(searchNotionImpl, { maxAge: 10000 })
